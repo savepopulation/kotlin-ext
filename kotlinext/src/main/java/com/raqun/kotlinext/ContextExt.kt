e6b33ffc3631eb7334124f7d37cc7d59264df76c
tyln <taylan.sbrcn@gmail.com>
2018-09-27 16:23:34 +0300

    context alert added

--- kotlinext/src/main/java/com/raqun/kotlinext/ContextExt.kt
+++ kotlinext/src/main/java/com/raqun/kotlinext/ContextExt.kt
@@ -2,6 +2,7 @@ package com.raqun.kotlinext
 
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 /**
  * Checks if a Broadcast can be resolved
@@ -12,3 +13,8 @@ fun Context.canResolveBroadcast(intent: Intent) = packageManager.queryBroadcastR
  * Checks if a Provider exists with given name
  */
 fun Context.providerExists(providerName: String) = packageManager.resolveContentProvider(providerName, 0) != null
+
+/**
+ * Creates a toast message with given message and duration
+ */
+fun Context.alert(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) = Toast.makeText(this, message, duration).show()
