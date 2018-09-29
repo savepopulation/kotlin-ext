ae658730ecebf0383784f87054a573ff3dd44c7b
tyln <taylan.sbrcn@gmail.com>
2018-09-29 17:19:44 +0300

    paste from clipboard ext added

--- kotlinext/src/main/java/com/raqun/kotlinext/EditTextExt.kt
+++ kotlinext/src/main/java/com/raqun/kotlinext/EditTextExt.kt
@@ -1,5 +1,7 @@
 package com.raqun.kotlinext
 
+import android.content.Context
+import android.text.TextUtils
 import android.widget.EditText
 import java.net.MalformedURLException
 import java.net.URL
@@ -20,4 +22,30 @@ fun EditText.getUrl(): URL? {
     } catch (e: MalformedURLException) {
         null
     }
+}
+
+/**
+ * Sets EditText text from Clipboard
+ */
+fun EditText.pasteFromClipBoard() {
+    var text = ""
+    val currentSdk = android.os.Build.VERSION.SDK_INT
+    if (currentSdk < android.os.Build.VERSION_CODES.HONEYCOMB) {
+        val manager: android.text.ClipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE)
+                as android.text.ClipboardManager
+        try {
+            text = manager.text.toString()
+        } catch (e: Exception) {
+            // ignored
+        }
+    } else {
+        val manager: android.content.ClipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE)
+                as android.content.ClipboardManager
+        manager.primaryClip?.let {
+            val item = manager.primaryClip.getItemAt(0)
+            text = item.text.toString()
+        }
+    }
+
+    if (!TextUtils.isEmpty(text)) setText(text)
 }
