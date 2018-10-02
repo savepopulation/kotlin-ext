9473f2185a1c2705244eceda87fddc07bf179204
tyln <taylan.sbrcn@gmail.com>
2018-10-02 19:48:24 +0300

    check service running ext added

--- kotlinext/src/main/java/com/raqun/kotlinext/ContextExt.kt
+++ kotlinext/src/main/java/com/raqun/kotlinext/ContextExt.kt
@@ -1,6 +1,8 @@
 package com.raqun.kotlinext
 
+import android.app.ActivityManager
 import android.content.Context
+import android.content.Context.ACTIVITY_SERVICE
 import android.content.Intent
 import android.widget.Toast
 
@@ -18,3 +20,16 @@ fun Context.providerExists(providerName: String) = packageManager.resolveContent
  * Creates a toast message with given message and duration
  */
 fun Context.alert(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) = Toast.makeText(this, message, duration).show()
+
+/**
+ * Checks if a service with given name is running.
+ */
+fun Context.isServiceRunning(serviceName: String): Boolean {
+    val manager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+    for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
+        if (serviceName == service.service.className) {
+            return true
+        }
+    }
+    return false
+}
