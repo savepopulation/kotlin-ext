67eeef65170173608b534921b3eb4c105f12ee29
tyln <taylan.sbrcn@gmail.com>
2018-10-19 07:58:42 +0300

    status and navigation bar hide ext added

--- kotlinext/src/main/java/com/raqun/kotlinext/ActivityExt.kt
+++ kotlinext/src/main/java/com/raqun/kotlinext/ActivityExt.kt
@@ -4,7 +4,11 @@ import android.app.Activity
 import android.content.Context
 import android.content.res.Configuration
 import android.support.v7.app.AlertDialog
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
 
 /**
  * Force close keyboard
@@ -28,4 +32,15 @@ inline fun Activity.alert(body: AlertDialog.Builder.() -> AlertDialog.Builder) {
     AlertDialog.Builder(this)
             .body()
             .show()
+}
+
+/**
+ * Hides Status and Navigation Bar
+ */
+fun Activity.hideStatusAndNavigationBar() {
+    window.decorView?.let {
+        it.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN;
+    }
+
+    actionBar?.hide()
 }
