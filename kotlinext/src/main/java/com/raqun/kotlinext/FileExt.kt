84e97d74a8ebeb4c66846855655c4e655a0c2585
tyln <taylan.sbrcn@gmail.com>
2018-11-04 18:04:52 +0300

    delete and clean directory implemented

--- kotlinext/src/main/java/com/raqun/kotlinext/FileExt.kt
+++ kotlinext/src/main/java/com/raqun/kotlinext/FileExt.kt
@@ -6,6 +6,8 @@ import android.os.Build
 import android.os.Environment
 import android.support.v4.content.FileProvider
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -78,4 +80,66 @@ fun File.getFolderSize(): Long {
     }
 
     return size
+}
+
+/**
+ * Deletes given directory and returns result
+ */
+fun File.deleteDirectory(): Boolean {
+    if (!exists()) return false
+    return cleanDirectory()
+}
+
+/**
+ * Force deletes given directory or file
+ */
+private fun File.forceDelete() {
+    if (isDirectory) {
+        deleteDirectory()
+    } else {
+        val filePresent = exists()
+        if (!delete()) {
+            if (!filePresent) {
+                throw FileNotFoundException("File does not exist: $this")
+            }
+            val message = "Unable to delete file: $this"
+            throw IOException(message)
+        }
+    }
+}
+
+/**
+ * Cleans directory and returns result
+ */
+private fun File.cleanDirectory(): Boolean {
+    val files = verifiedDirectoryFiles()
+    var allFilesDeleted = true
+    for (file in files) {
+        try {
+            forceDelete()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            if (allFilesDeleted) allFilesDeleted = false
+        }
+    }
+    return allFilesDeleted
+}
+
+/**
+ * Verifies files of directory
+ */
+private fun File.verifiedDirectoryFiles(): Array<File> {
+    if (!exists()) {
+        val message = toString() + " does not exist"
+        throw IllegalArgumentException(message)
+    }
+
+    if (!isDirectory) {
+        val message = toString() + " is not a directory"
+        throw IllegalArgumentException(message)
+    }
+
+    return listFiles()
+            ?: // null if security restricted
+            throw IOException("Failed to list contents of $this")
 }
