a2f22be796a2a6cd79f4b7932d609886ca4403cd
tyln <taylan.sbrcn@gmail.com>
2019-01-24 19:03:24 +0300

    any try catch ext added

--- kotlinext/src/main/java/com/raqun/kotlinext/AnyExt.kt
+++ kotlinext/src/main/java/com/raqun/kotlinext/AnyExt.kt
@@ -9,3 +9,18 @@ fun Any?.isNotNull() = this != null
  * Checks if any object is null
  */
 fun Any?.isNull() = this == null
+
+/**
+ * Try-catch
+ */
+fun Any.tryCatch(tryBlock: () -> Unit,
+                 catchBlock: ((t: Throwable) -> Unit)? = null,
+                 finalBlock: (() -> Unit)?) {
+    try {
+        tryBlock()
+    } catch (e: Exception) {
+        catchBlock?.invoke(e)
+    } finally {
+        finalBlock?.invoke()
+    }
+}
